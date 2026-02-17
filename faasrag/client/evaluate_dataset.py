fd7728177a5ec54c9b616c012e96c4642af7d6c3
pw-02 <watters.patrick@outlook.com>
2026-02-17 12:42:10 -0800

    stage2_max_phrases

--- faasrag/client/evaluate_dataset.py
+++ faasrag/client/evaluate_dataset.py
@@ -426,7 +426,7 @@ def main(cfg: RagServiceConfig):
 
     # Stage-2 knobs
     parser.add_argument("--stage2_alpha", type=float, default=0.2)
-    parser.add_argument("--stage2_max_phrases", type=float, default=10)
+    parser.add_argument("--stage2_max_phrases", type=float, default=5)
     parser.add_argument("--stage2_hybrid_prompt", action="store_true")
 
     args = parser.parse_args()
