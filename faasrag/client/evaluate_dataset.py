9cd0d96543af6c05a39915c0f872ff894dd71ada
pw-02 <watters.patrick@outlook.com>
2026-02-18 12:57:40 -0800

    c1

--- faasrag/client/evaluate_dataset.py
+++ faasrag/client/evaluate_dataset.py
@@ -109,8 +109,8 @@ RESULT_COLUMNS: List[str] = [
     "mode",
     # cfg (prefixed)
     "cfg_limit",
-    "cfg_print_first_n",
-    "cfg_tqdm_update_every",
+    # "cfg_print_first_n",
+    # "cfg_tqdm_update_every",
     "cfg_top_k",
     "cfg_logit_max_mined_candidates",
     "cfg_logit_bias_strength",
