1af1fc4f4a495d86a8d9eeee0e9aab57dbbe36f0
pw-02 <watters.patrick@outlook.com>
2026-03-03 23:39:34 -0800

    v1

--- pwrag/client/eval_dataset.py
+++ pwrag/client/eval_dataset.py
@@ -6,9 +6,9 @@ from __future__ import annotations
 
 import os
 os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
-os.environ["VLLM_ATTENTION_BACKEND"] = "FLEX_ATTENTION"
+# os.environ["VLLM_ATTENTION_BACKEND"] = "FLEX_ATTENTION"
 # os.environ["TOKENIZERS_PARALLELISM"] = "false"  # optional
-print("backend:", os.environ.get("VLLM_ATTENTION_BACKEND"))
+# print("backend:", os.environ.get("VLLM_ATTENTION_BACKEND"))
 from typing import Optional
 
 import hydra
--- pwrag/generator/generator.py
+++ pwrag/generator/generator.py
@@ -409,7 +409,6 @@ class VLLMGenerator(BaseGenerator):
                 gpu_memory_utilization = self.gpu_memory_utilization,
                 max_logprobs = 32016,
                 max_model_len = self.max_model_len,
-                enforce_eager = True, # enforce_egar can help with vLLM's sometimes generation cut off issue, but it may cause some performance drop. So we set it to True by default but you can set it to False if you want.
             )
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_path, trust_remote_code=True)
         # if "llama" in self.model_path.lower():
