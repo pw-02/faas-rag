317acda36253b2e50c98fbcb2d71fe03b76f2a53
pw-02 <watters.patrick@outlook.com>
2026-03-05 23:21:52 -0800

    c

--- pwrag/server/start_llm_server.py
+++ pwrag/server/start_llm_server.py
@@ -19,7 +19,7 @@ def start_server(cfg: AppConfig) -> None:
         "--port", port,
         "--gpu-memory-utilization", "0.95",
         "--max-model-len", "20000",
-        "--generation-config", "vllm",
+        # "--generation-config", "vllm",
 
     ]
 
